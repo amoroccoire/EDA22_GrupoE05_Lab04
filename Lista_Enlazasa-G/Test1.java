e576465337be766a6ba89f836871e46f0b884d3e
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-06-11 18:41:19 -0500

    Se modifica el Test1 el método de insertSort(), aún no funciona,se ha dado una condición en la inserción del key y se corrigio errores pequeños.

--- Lista_Enlazasa-G/Test1.java
+++ Lista_Enlazasa-G/Test1.java
@@ -14,21 +14,32 @@ public class Test1 {
     lint.insertFirst(25);
     System.out.println(lint);
     lint.remove(9);
-    System.out.println(lint);
+    System.out.println(lint + "\n---------------------------------");
     insertionSort(lint);
   }
   public  static <E extends Comparable<E>> void insertionSort(LinkedList<E> A) {
     Node<E> key;
     int i;
-    for (int j=1; j<A.getTamaño(); j=j+1) {
+    for (int j=1; j<A.getTamaño(); j++) {
       key = A.get(j);
       i=j-1;
-      while(i>-1 && A.get(i).getData().compareTo(key.getData())<0) {
-	A.remove(i+1);
+      while(i >= 0 && A.get(i).getData().compareTo(key.getData())>0) {	
+	A.get(i).setNext(A.get(i+1).getNext());
+	A.insert(A.get(i).getData(), i+1);
 	i = i-1;
       }
       System.out.println(A);
-      A.get(i).setNext(key);
+      if(i==-1){
+	A.insertFirst(key.getData());
+	A.remove(1);
+	System.out.println("Listo");
+      }
+      else{
+	Node<E> aux;
+	aux = A.get(i+1).getNext();
+	key.setNext(aux);
+	A.get(i).setNext(key);
+      }
     }
   }
 
