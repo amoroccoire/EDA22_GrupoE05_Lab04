80da69bd050bdc0df14bd3ba3118baa4e3bc048a
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-06-11 20:41:50 -0500

    Se modifican los dos métodos creados para que se pueda contabilizar el tiempo de ejecución.

--- Lista_Enlazasa-G/Test1.java
+++ Lista_Enlazasa-G/Test1.java
@@ -9,14 +9,19 @@ public class Test1 {
     System.out.print( "Introduzca el tamaño del arreglo: " );
     tamano = teclado.nextInt();
 
-    LinkedList<Integer> lint = generarPeorCaso(tamano);
-    System.out.println(lint);
-    System.out.println("-------------------");
-    insertionSort(lint);
+    ArrayList<LinkedList<Integer>> casos = new ArrayList<LinkedList<Integer>>();
+    for(int n=1; n<=tamano; n++) {
+    	casos.add(generarPeorCaso(n));
+    }
+    Iterator<LinkedList<Integer>> puntero = casos.iterator();
+    while(puntero.hasNext()){
+    	System.out.println(insertionSortTime(puntero.next()));
+    }
   }
-  public  static <E extends Comparable<E>> void insertionSort(LinkedList<E> A) {
+    public  static <E extends Comparable<E>> long insertionSortTime(LinkedList<E> A) {
     Node<E> key;
     int i;
+    long nano_startTime = System.nanoTime();
     for (int j=1; j<A.getTamaño(); j++) {
       key = A.get(j);
       i = j - 1;
@@ -25,7 +30,7 @@ public class Test1 {
 	A.insert(A.get(i).getData(), i+2);
 	i--;
       }
-      System.out.println(A);
+      //System.out.println(A);
       if(i == -1){
 	A.insertFirst(key.getData());
 	A.remove(1);
@@ -37,15 +42,45 @@ public class Test1 {
 	A.get(i).setNext(key);
       }
       
-     System.out.println("Lista ordenada: " + A);
+     //System.out.println("Lista ordenada: " + A);
     }
-  }
-  public static  LinkedList<Integer> generarPeorCaso(int t) {
-    LinkedList<Integer> lista= new LinkedList<Integer>();
-    for (int i = 0; i < t; i++) {
-      lista.insertLast(t-i-1); 
+
+    long nano_endTime = System.nanoTime();
+    return nano_endTime - nano_startTime; 
     }
-    System.out.println(lista.getTamaño());
-    return lista;
-  }
+
+    public  static <E extends Comparable<E>> void insertionSort(LinkedList<E> A) {
+      Node<E> key;
+      int i;
+      for (int j=1; j<A.getTamaño(); j++) {
+	key = A.get(j);
+	i = j - 1;
+	while(i >= 0 && A.get(i).getData().compareTo(key.getData())>0) {	
+	  A.get(i).setNext(A.get(i+1).getNext());
+	  A.insert(A.get(i).getData(), i+2);
+	  i--;
+	}
+	//System.out.println(A);
+	if(i == -1){
+	  A.insertFirst(key.getData());
+	  A.remove(1);
+	}
+	else{
+	  Node<E> aux;
+	  aux = A.get(i+1).getNext();
+	  key.setNext(aux);
+	  A.get(i).setNext(key);
+	}
+
+	//System.out.println("Lista ordenada: " + A);
+      }
+    }
+    public static  LinkedList<Integer> generarPeorCaso(int t) {
+      LinkedList<Integer> lista= new LinkedList<Integer>();
+      for (int i = 0; i < t; i++) {
+	lista.insertLast(t-i-1); 
+      }
+      //System.out.println(lista.getTamaño());
+      return lista;
+    }
 }
