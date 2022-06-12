930d2e4dc8893b268668c91961736d6cb1b58988
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-06-12 15:21:15 -0500

    se permite flujo de datos y se escriben los tiempos del ordenamiento en archivo2.txt

--- DobleListaEnlazada/ListaDoble.java
+++ DobleListaEnlazada/ListaDoble.java
@@ -1,15 +1,33 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class ListaDoble{
-    public static void main(String[] args){
-        DoubleLinkedList<Integer> listas = new DoubleLinkedList<Integer>();
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+public class ListaDoble {
+
+    public static void main(String[] args) throws FileNotFoundException {
+       
         Scanner sc = new Scanner(System.in);
-        System.out.print("Tamaño de la lista: ");
-        int tamanio = sc.nextInt();
+        System.out.print("Número de arreglos: ");
+        int num = sc.nextInt();
 
-        for (int i = 0; i < tamanio; i++)
-            lista.push(tamanio - i);
+        ArrayList<DoubleLinkedList> listasD = new ArrayList<DoubleLinkedList>();
+        String archivo = "archivo1.txt";
+        PrintWriter oS = new PrintWriter(archivo);
 
-        System.out.print(lista.toString());
+        for (int i = 1; i <= num; i++) {
+            DoubleLinkedList<Integer> lista = new DoubleLinkedList<Integer>();
+            for (int j = 0; j < i; j++)
+                lista.push(i - j);
+
+            listasD.add(lista);
+        }
+
+        for (int i = 0; i < listasD.size(); i++)
+            oS.println(String.valueOf(listasD.get(i).ordenamientoInsercion()));
+
+        oS.close();       
+    }
 }
+
